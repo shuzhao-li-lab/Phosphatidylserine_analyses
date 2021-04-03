6fd67032ec8fd64e03be346179546ac301667590
Minghao Gong <gmhhope@gmail.com>
2021-04-03 15:27:16 -0400

    mcg 0403-rerun on both C18neg & HILICpos

---
+++ code/bash/mcg_bash/mcg_04032021_HILICpos.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+I_PATH="/Users/gongm/Documents/projects/Rafi_Ahmed_exhaustedCD8Tcell/data/output/HILICpos_0402/ttest_equal/"
+
+O_PATH="/Users/gongm/Documents/projects/Rafi_Ahmed_exhaustedCD8Tcell/data/output/HILICpos_0402/mcg/"
+
+
+mkdir $O_PATH
+
+cd $O_PATH
+
+FILE_NAME="ttest_res_padj_R5posvsR5neg.txt"
+mummichog -f $I_PATH$FILE_NAME -o mcg_HILICpos_padj_default_R5posvsR5neg
+mummichog -f $I_PATH$FILE_NAME -c 0.25 -o mcg_HILICpos_padj_025_R5posvsR5neg
+
+FILE_NAME="ttest_res_padj_R5posvsNaive.txt"
+mummichog -f $I_PATH$FILE_NAME -o mcg_HILICpos_padj_default_R5posvsNaive
+mummichog -f $I_PATH$FILE_NAME -c 0.25 -o mcg_HILICpos_padj_025_R5posvsNaive
+
+FILE_NAME="ttest_res_padj_R5negvsNaive.txt"
+mummichog -f $I_PATH$FILE_NAME -o mcg_HILICpos_padj_default_R5negvsNaive
+mummichog -f $I_PATH$FILE_NAME -c 0.25 -o mcg_HILICpos_padj_025_R5negvsNaive
+
+
+
+
+
+FILE_NAME="ttest_res_rawpval_R5posvsR5neg.txt"
+mummichog -f $I_PATH$FILE_NAME -o mcg_HILICpos_RawPval_default_R5posvsR5neg
+mummichog -f $I_PATH$FILE_NAME -c 0.05 -o mcg_HILICpos_RawPval_005_R5posvsR5neg
+
+
+FILE_NAME="ttest_res_rawpval_R5negvsNaive.txt"
+mummichog -f $I_PATH$FILE_NAME -o mcg_HILICpos_RawPval_default_R5negvsNaive
+mummichog -f $I_PATH$FILE_NAME -c 0.05 -o mcg_HILICpos_RawPval_005_R5negvsNaive
+
+
+FILE_NAME="ttest_res_rawpval_R5posvsNaive.txt"
+mummichog -f $I_PATH$FILE_NAME -o mcg_HILICpos_RawPval_default_R5posvsNaive
+mummichog -f $I_PATH$FILE_NAME -c 0.05 -o mcg_HILICpos_RawPval_005_R5posvsNaive
+
