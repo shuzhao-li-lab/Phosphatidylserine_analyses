560a8fcf687de1832b76814c25f9b7532d64a192
Minghao Gong <gmhhope@gmail.com>
2021-06-13 21:03:56 -0400

    using docker img spectra_xcms:version2 instead of previous one. The docker img has been uploaded to dockerhub.

--- code/bash/docker_run_SpectraTutorial4_MS2analysis.sh
+++ code/bash/docker_run_SpectraTutorial4_MS2analysis.sh
@@ -2,4 +2,4 @@ docker run \
     -e PASSWORD=bioc \
     -p 8787:8787 \
     -v /Users/gongm/Documents/projects/Rafi_Ahmed_exhaustedCD8Tcell:/home/rstudio/external \
-    spectra_xcms:latest
+    spectra_xcms:version2
