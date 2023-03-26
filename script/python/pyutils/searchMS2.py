bb31232befeff0ccfb7348b57c048cf20bf653ea
Minghao Gong <gmhhope@gmail.com>
2023-03-25 21:20:14 -0400

    MS2 annotations tested - awaiting Joshua sent me back the updated file - Thus testing on the old data - Have more dedicated functions to annotate on the annot_dict (see `Incorporate lipidMAPS...` - `searchMS2.py` update functions to export spectrum in a customized and savable manner; then based on that, further revise NL calculation, precursor_in_ms2 and filter based on precursor_in_ms2 with the feature m/z.   - This proves to be important as sometimes precursor might be contaminated by multiple ions, and thus precursor m/z no longer precise. The precursor_in_ms2 if presents, becomes much more convincing for estimate ppm.   - One example is PS 40:5 & LPS 41:5, which can be closed in m/z (diff ~ 0.3 m/z). The precursor m/z can be closed to LPS, but in reality the precursor presents in ms2 spectra is exactly point to PS 40:5. - `multi-files v2` basically rerender results based on the new functions in `searchMS2.py` and it is now more automatic.

--- script/python/pyutils/searchMS2.py
+++ script/python/pyutils/searchMS2.py
@@ -4,6 +4,18 @@ import pymzml
 import os
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
+
+def subset_specs(exp,
+                 rt_range):
+    rt_scans = [round(spec.scan_time[0]*60,2) for spec in exp]
+    ID_lower = next((i for i, x in enumerate(rt_scans) if x >= rt_range[0] ), None)
+    ID_upper = next((i for i, x in enumerate(rt_scans) if x >= rt_range[1] ), None)
+    sub_exp = []
+    for spec in exp:
+        if ID_lower < spec.ID < ID_upper:
+            sub_exp.append(spec)
+    return(sub_exp)
 
 # https://github.com/shuzhao-li/asari/blob/284d49db05bc95377072a865663550f59340aca3/asari/tools/plot.py
 def get_potential_precursor_from_file(infile, 
@@ -104,8 +116,6 @@ def get_potential_precursor_from_exp_filtbyRt(exp,
 
 
 
-import matplotlib.pyplot as plt
-
 def plot_spectra(spectra, 
                  save_figure = False,
                  output_path = "./",
@@ -133,7 +143,7 @@ def plot_spectra(spectra,
             ax.text(x, y, f"{round(x,2)}", ha='left', va='bottom', rotation=45, fontsize=16)
 
         # Set the plot title and axis labels
-        ax.set_title(f"MS2 scan of {round(precursor_mz,4)}_{round(spectrum.scan_time[0]*60,1)}", fontsize = 20)
+        ax.set_title(f"MS2 scan of {round(precursor_mz,4)}_{round(spectrum.scan_time[0]*60,2)}", fontsize = 20)
         ax.set_xlabel("m/z")
         ax.set_ylabel("Intensity")
 
@@ -154,11 +164,130 @@ def plot_spectra(spectra,
         # Show the plot (optional)
         plt.show()
         
+
+def export_spectrum(spectrum):
+    '''
+    Export spectrum from pymzML spectrum object
+    Return m/z, precursor, its spectrum index, estimated time
+    
+    '''
+    res_dict = {
+            "Id": spectrum.ID,
+            "precursor_mz" : round(spectrum.selected_precursors[0]['mz'], 4) if hasattr(spectrum, "selected_precursors") else "",
+            "mzs" : spectrum.mz.tolist(),
+            "intensities" : spectrum.i.tolist(),
+            "est_time" : round(spectrum.scan_time[0]*60,2)
+    }
+    
+    return res_dict 
+
+
+def plot_spectrum_from_list_of_dict(spec_dicts, 
+                 save_figure = False,
+                 output_path = "./",
+                 label = ""):
+    for spec_dict in spec_dicts:
         
+        mz_values = spec_dict["mzs"]
+        intensity_values = spec_dict["intensities"]
+        Id = spec_dict["Id"]
+        intensity_values = [x/max(intensity_values) for x in intensity_values]
+        
+        zipped_mz_int = [(x,y) for x,y in zip(mz_values,intensity_values) if y > 0.05]
+        mz_values, intensity_values = zip(*zipped_mz_int)
+        
+        precursor_mz = spec_dict['precursor_mz']
+        est_time = spec_dict['est_time']
+        
+        # Create the plot figure with more height
+        fig, ax = plt.subplots(figsize=(8, 6))
+
+        # Create the bar plot
+        ax.bar(mz_values, intensity_values, width=2)
+
+        # Add the annotation text to the top of each bar
+        for x, y in zip(mz_values, intensity_values):
+            ax.text(x, y, f"{round(x,2)}", ha='left', va='bottom', rotation=45, fontsize=16)
+
+        # Set the plot title and axis labels
+        ax.set_title(f"MS2 scan of {round(precursor_mz,4)}_{est_time}", fontsize = 20)
+        ax.set_xlabel("m/z")
+        ax.set_ylabel("Intensity")
+
+        # Set the x-axis limits to 1-1000
+        ax.set_xlim(1, 900)
+
+        # Adjust the y-axis limits to leave some space at the top
+        ax.set_ylim(top=ax.get_ylim()[1]*1.2)
+
+        # Save the plot as PDF
+        if save_figure == True:
+            try:
+                os.mkdir(output_path)
+            except:
+                None
+            fig.savefig(os.path.join(output_path,f"{label}_ms2_scan_{precursor_mz}_{est_time}_Scan#{Id}.pdf"))
+
+        # Show the plot (optional)
+        plt.show()
+
         
 def cal_ppm(mz1,
             mz2):
     return abs((mz1-mz2))*1000000/mz2
+
+
+
+
+def find_in_ms2_precursor(spec,ppm_prec = 100, present = 0.05):
+    '''
+    This function assumeds that the precursor is the maximum of the spec and have at least 5% intensities over the highest one.
+    '''
+    
+    # find the peaks that have at least 5% of the top one
+    int4search = [x for x in spec['intensities'] if x/max(spec['intensities']) > present]
+    
+    index_int4search = [i for i,x in enumerate(spec['intensities']) if x in int4search]
+    int_filt_mzs = [spec['mzs'][ii] for ii in index_int4search]
+    
+    mz_delta = spec['precursor_mz']*10**(-6)*ppm_prec
+    prec_mz_range = [spec['precursor_mz'] - mz_delta, spec['precursor_mz'] + mz_delta]
+    potential_prec_mzs = [x for x in int_filt_mzs if x > prec_mz_range[0] and x < prec_mz_range[1]]
+    sel_prec_mzs = []
+    ppms = []
+    for mz in potential_prec_mzs:
+        ppm = cal_ppm(spec['precursor_mz'],mz)
+        if  ppm < ppm_prec:
+            sel_prec_mzs.append(mz)
+            ppms.append(ppm)
+    if len(sel_prec_mzs) > 0:
+        final_decided_mz = round(sel_prec_mzs[ppms.index(max(ppms))],5)
+    else:
+        final_decided_mz = np.nan
+    return(final_decided_mz)
+
+def search_NL_customSpecObj(spectra,
+              NL_mz = 87.03124,
+              ppm_prec = 100,
+              ppm = 40):
+    res_data = []
+
+    for spec in spectra:
+        in_ms2_precursor = find_in_ms2_precursor(spec,ppm_prec = 100, present = 0.05)
+        if np.isnan(in_ms2_precursor):
+            sel_prec_mz = spec['precursor_mz'] # this m/z will not be exactly the precursor m/z    
+        else:
+            sel_prec_mz = in_ms2_precursor
+        
+        sel_mz = [mz for mz in spec.mz if abs(mz - sel_prec_mz) < (np.ceil(NL_mz)+1)] # +1 is to increase a little bit the range
+    
+        for mz in sel_mz:
+            calc_ppm = cal_ppm((sel_prec_mz - mz),NL_mz)
+            if calc_ppm < ppm:
+                print(calc_ppm)
+                res_data.append(spec)
+                break
+    return res_data
 
 # this function only works for situation where you look at charge state = 1
 def search_NL(spectra,
@@ -182,3 +311,10 @@ def search_NL(spectra,
                 res_data.append(spec)
                 break
     return res_data
+
+# a step to avoid issue with precursor being too far away from mz
+def check_annot_dict_precursorMzVsfeatureMz(annot_dict,id2check):
+    '''
+    Given an annotation dictionary, which has m/z 
+    '''
+    return cal_ppm(annot_dict[id2check]['[M-H+e]'],np.mean(annot_dict[id2check]['annotation_evidence']['MS2'][0]['precursor']))
